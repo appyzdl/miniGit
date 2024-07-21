5b455779db774e035ae2dbd7599bda9ea2f4b3ae
appyz <appyzdl5@gmail.com>
2024-07-22 00:19:17 +0530

    basic setup completed

---
+++ lib/repository.py
@@ -0,0 +1,35 @@
+import os
+import configparser
+
+
+class GitRepository (object):
+    """A git repository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        # Check if the directory is a Git repository
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository %s" % path)
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        # Check if the configuration file exists
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        # Check the repository format version
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(
+                    "Unsupported repositoryformatversion %s" % vers)
